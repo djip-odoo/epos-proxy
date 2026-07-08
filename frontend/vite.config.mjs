1578b6f5aff5c27e24c0c40d995743fcecb52ffd
dhruv <djip@odoo.com>
2026-07-08 12:53:28 +0530

    draft

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -5,15 +5,17 @@ import wails from "@wailsio/runtime/plugins/vite";
 import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        vuePlugin(),
-        wails("./bindings"),
-        legacy({
-          targets: [
-            "Chrome 83",
-            "Android >= 10",
-          ],
-        }),
-    ],
+  plugins: [
+    tailwindcss(),
+    vuePlugin(),
+    wails("./bindings"),
+    legacy({
+      targets: ["Chrome >= 40", "Android >= 5"],
+      modernTargets: ["Chrome >= 61"],
+      modernPolyfills: true,
+    }),
+  ],
+  build: {
+    cssTarget: "chrome40",
+  },
 })
